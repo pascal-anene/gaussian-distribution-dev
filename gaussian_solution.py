e883474253596904a28ee137b9a5be345cc9a033
Pascal Anene <pascal.o.anene@gmail.com>
2022-10-15 16:36:00 +0200

    defined magic method for addition two gaussian distributions:

--- gaussian_solution.py
+++ gaussian_solution.py
@@ -160,6 +160,27 @@ class Gaussian():
 
             return x, y
 
-            
+
+        # Below Magic Methods are defined for default Addition and Representation 
+
+        def __add__(self, other):
+            """Function to add together two Gaussian distributions
+
+            Args:
+                other (Gaussian): Gaussian instance
+
+            Returns: 
+                Gaussian: Gaussian distribution
+            """
+            result = Gaussian()
+            result.mean = self.mean + other.mean
+            result.stdev = math.sqrt(self.stdev ** 2 + other.stdev ** 2)
+
+            return result
+
+        def 
+
+
+
 
 
